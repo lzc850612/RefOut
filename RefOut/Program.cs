34c95e8f2af2b6221a11112dd8f3b3c6c90b38ae
ted <lzc850612@gmail.com>
2013-10-14 21:43:49 +0800

    ref out example

---
+++ RefOut/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 a = new Class1 { a = 123, b = 456 };
+            Test(ref a);
+            Console.Write(a.a);
+            Console.Read();
+        }
+
+        private static void Test(ref Class1 input)
+        {
+            input = new Class1();
+            input.a = 234;
+            Console.Write(input.a);
+        }
+    }
+
+    public class Class1
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+    }
+}
